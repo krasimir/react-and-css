fcadab0daf08246717b7337df4d99d613fb5aea5
Krasimir Tsonev <krasimir@outset.ws>
2016-03-08 08:46:05 +0200

    Setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <h1>Hello world</h1>
+    );
+  }
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/app.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/build',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
